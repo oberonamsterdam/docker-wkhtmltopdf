6e2d63eb50370084fe2f6c0ed9dc2b7f09e8190c
Martin Reguly <martin.reguly@gmail.com>
2021-12-19 14:22:07 +0100

    Create app.py

---
+++ ubuntu20-python3/app.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+"""
+    WSGI APP to convert wkhtmltopdf As a webservice
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+import json
+import tempfile
+import subprocess
+
+from werkzeug.wsgi import wrap_file
+from werkzeug.wrappers import Request, Response
+from executor import execute
+
+
+@Request.application
+def application(request):
+    """
+    To use this application, the user must send a POST request with
+    base64 or form encoded encoded HTML content and the wkhtmltopdf Options in
+    request data, with keys 'base64_html' and 'options'.
+    The application will return a response with the PDF file.
+    """
+    if request.method != 'POST':
+        return
+
+    request_is_json = request.content_type.endswith('json')
+    with tempfile.NamedTemporaryFile(suffix='.html', mode='w+b') as source_file:
+        options = {}
+        if request_is_json:
+            # If a JSON payload is there, all data is in the payload
+            payload = json.loads(request.data.decode())
+            source_file.write(payload['contents'].encode())
+            options = payload.get('options', {})
+        elif request.files:
+            # First check if any files were uploaded
+            pdf_file = request.files['file']
+            f_content = pdf_file.read()
+            source_file.write(f_content)
+
+            # Load any options that may have been provided in options
+            options = json.loads(request.form.get('options', '{}'))
+
+        source_file.flush()
+
+        # Evaluate argument to run with subprocess
+        args = ['/usr/bin/xvfb-run', 'wkhtmltopdf']
+
+        # Add Global Options
+        if options:
+            for option, value in options.items():
+                args.append('--%s' % option)
+                if option == 'header-html':
+                    subprocess.call(["wget", value, "-O", "/tmp/header.html"])
+                    value = "/tmp/header.html"
+
+                if option == 'footer-html':
+                    subprocess.call(["wget", value, "-O", "/tmp/footer.html"])
+                    value = "/tmp/footer.html"
+
+                if value:
+                    args.append('"%s"' % value)
+
+        # Add source file name and output file name
+        file_name = source_file.name
+        args += [file_name, file_name + ".pdf"]
+        # Execute the command using executor
+        execute(' '.join(args))
+
+        return Response(
+            wrap_file(request.environ, open(file_name + '.pdf', 'r+b')),
+            mimetype='application/pdf',
+        )
+
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+
+    run_simple(
+        '127.0.0.1', 15000, application, use_debugger=True, use_reloader=True
+    )
